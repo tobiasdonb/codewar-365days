d16cf34d3bd3045202cb72709b973f36f5f893ba
tobiasdonb <tobiasprogrammer@gmail.com>
2024-04-13 23:41:58 +0800

    day #2

--- 4.smash-sentence.js
+++ 4.smash-sentence.js
@@ -10,10 +10,10 @@
 //   return hasil;
 // }
 
-// function smash(words) {
-//   return words.join(' ');
-// }
+function smash(words) {
+  return words.join(' ');
+}
 
-// const smash = (words) => words.join(' ')
+const smash = (words) => words.join(' ');
 
 console.log(smash(['this', 'is', 'a', 'really', 'long', 'sentence']));
